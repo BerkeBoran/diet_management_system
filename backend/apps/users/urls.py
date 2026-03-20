92c1bb57f9472f5be2027b8d32643b87c24b838b
BerkeBoran <2022141046@cumhuriyet.edu.tr>
2026-03-20 16:27:27 +0300

    chore: add register endpoints to users urls

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.users.views import MyTokenObtainPairView
+from apps.users.views.register import ClientRegisterView, DieticianRegisterView
 
 urlpatterns = [
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/client/', ClientRegisterView.as_view(), name='client_register'),
+    path('register/dietician/', DieticianRegisterView.as_view(), name='dietician_register'),
 
 ]
