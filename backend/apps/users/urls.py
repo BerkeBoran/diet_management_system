77e62bcc03cd128d6d4f98f8f44b7e0ac90aae67
BerkeBoran <2022141046@cumhuriyet.edu.tr>
2026-03-20 17:05:07 +0300

    chore: add profile url

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -3,11 +3,13 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.users.views import MyTokenObtainPairView
 from apps.users.views.register import ClientRegisterView, DieticianRegisterView
+from apps.users.views.users import ProfileView
 
 urlpatterns = [
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/client/', ClientRegisterView.as_view(), name='client_register'),
     path('register/dietician/', DieticianRegisterView.as_view(), name='dietician_register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 
 ]
