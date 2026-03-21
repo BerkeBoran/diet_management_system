b928f01cb70b0b571c4f82593ae066c2d88d0786
BerkeBoran <2022141046@cumhuriyet.edu.tr>
2026-03-21 18:17:49 +0300

    chore: update users and config urls

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,15 +1,23 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.users.views import MyTokenObtainPairView
 from apps.users.views.register import ClientRegisterView, DieticianRegisterView
-from apps.users.views.users import ProfileView
+from apps.users.views.review import DieticianReviewView
+from apps.users.views.users import ProfileView, DieticianViewSet
+
+router = DefaultRouter()
+router.register('dieticians', DieticianViewSet, basename='dietician')
 
 urlpatterns = [
+    path('', include(router.urls)),
+
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/client/', ClientRegisterView.as_view(), name='client_register'),
     path('register/dietician/', DieticianRegisterView.as_view(), name='dietician_register'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('reviews/', DieticianReviewView.as_view(), name='dietician_review'),
 
 ]
