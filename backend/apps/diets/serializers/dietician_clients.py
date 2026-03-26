ac6e934f50e3a61571d4576d8697489985f602d8
BerkeBoran <2022141046@cumhuriyet.edu.tr>
2026-03-26 14:32:25 +0300

    feat: add dietician client detail serializer and viewset

--- backend/apps/diets/serializers/dietician_clients.py
+++ backend/apps/diets/serializers/dietician_clients.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
 
 from apps.diets.models.assignment import DieticianAssignment
+from apps.users.models import Client
 
 
 class DieticianClientsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = DieticianAssignment
-        fields = ['id', 'goal', 'dietician', 'status', 'created_at','updated_at', 'duration']
+        fields = ['id', 'goal', 'dietician', 'status', 'created_at','updated_at', 'duration', 'accepted_at']
         read_only_fields = ['id', 'status','created_at', 'updated_at', 'goal', 'duration']
 
 
+class DieticianClientDetailSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='full_name',max_length=255)
+    class Meta:
+        model = Client
+        fields = ['name', 'allergies', 'height', 'weight', 'age', 'gender']
--- backend/apps/diets/views/dietician_clients.py
+++ backend/apps/diets/views/dietician_clients.py
@@ -2,7 +2,8 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from apps.diets.models.assignment import DieticianAssignment
-from apps.diets.serializers.dietician_clients import DieticianClientsSerializer
+from apps.diets.serializers.dietician_clients import DieticianClientsSerializer, DieticianClientDetailSerializer
+from apps.users.models import Client
 
 
 class DieticianClientsViewSet(viewsets.ModelViewSet):
@@ -15,3 +16,12 @@ class DieticianClientsViewSet(viewsets.ModelViewSet):
         if hasattr(user, 'dietician'):
             return DieticianAssignment.objects.filter(dietician=user, status__in=[DieticianAssignment.Status.INPROGRESS, DieticianAssignment.Status.ENDED])
 
+
+class DieticianClientDetailViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = DieticianClientDetailSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if hasattr(user, 'dietician'):
+            return Client.objects.filter(assignments__dietician=user)
