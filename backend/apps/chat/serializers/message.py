c64ad7d87024e6f71435448afc84187e30b33035
BerkeBoran <2022141046@cumhuriyet.edu.tr>
2026-04-02 14:51:13 +0300

    refactor: merge dietician title into sender_name and remove get_title

--- backend/apps/chat/serializers/message.py
+++ backend/apps/chat/serializers/message.py
@@ -7,38 +7,25 @@ class MessageSerializer(serializers.ModelSerializer):
 
     sender_id = serializers.IntegerField(source='sender.id', read_only=True)
     sender_name = serializers.SerializerMethodField(read_only=True)
-    sender_title = serializers.SerializerMethodField(read_only=True)
     is_mine = serializers.SerializerMethodField()
     timestamp = serializers.DateTimeField(format='%H:%M', read_only=True)
 
     class Meta:
         model = Message
-        fields = ('id', 'sender_id', 'sender_title', 'sender_name', 'content', 'timestamp', 'is_read', 'is_mine')
+        fields = ('id', 'sender_id','sender_name', 'content', 'timestamp', 'is_read', 'is_mine')
 
 
     def get_sender_name(self, obj):
-        if obj.sender:
-            return obj.sender.full_name
-        return "Silinmiş Kullanıcı"
+        if hasattr(obj.sender, 'dietician'):
+            return f"{obj.sender.dietician.get_title_display()} {obj.sender.full_name}"
+        return f"{obj.sender.full_name}"
+
 
     def get_is_mine(self, obj):
         request = self.context.get('request')
         if request and request.user.is_authenticated:
             return obj.sender == request.user
         return False
-
-    def get_sender_title(self, obj):
-        sender = obj.sender
-
-        if not sender:
-            return None
-
-        if hasattr(sender, 'dietician') and sender.dietician:
-
-            if getattr(sender.dietician, 'title', None):
-                return sender.dietician.get_title_display()
-
-        return None
 
 
 
