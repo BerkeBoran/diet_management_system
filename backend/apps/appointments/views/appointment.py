f2a99096d52d40b90c8ed8e37cbadaa7761713c7
BerkeBoran <2022141046@cumhuriyet.edu.tr>
2026-04-16 15:06:17 +0300

    fix(views): fix booked_times calculation in daily_slots

--- backend/apps/appointments/views/appointment.py
+++ backend/apps/appointments/views/appointment.py
@@ -17,51 +17,71 @@ class AppointmentViewSet(viewsets.ModelViewSet):
     serializer_class = AppointmentSerializer
     permission_classes = [IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save()
+    def create(self, request, *args, **kwargs):
+        dietician = request.data.get('dietician')
+        date = request.data.get('date')
+        start_time = request.data.get('start_time')
 
+        is_taken = Appointment.objects.filter(
+            dietician=dietician,
+            date=date,
+            start_time=start_time,
+            status = 'SCHEDULED',
+        ).exists()
+
+        if is_taken:
+            return Response({
+                "error": "Diyetisyenin bu saatte başka bir randevusu var"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        return super().create(request, *args, **kwargs)
 
 class DieticianAvailabilityViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = AvailabilitySerializer
     queryset = Availability.objects.all()
 
-    @action(methods=['get'], detail=True)
-    def daily_slots(self, request, pk=None):
+    @action(methods=['get'], detail=False)
+    def daily_slots(self, request):
+        dietician = request.query_params.get('dietician')
         date_str = request.query_params.get('date')
         if not date_str:
             return Response({'error': 'Tarih gerekli'}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
             target_date = datetime.strptime(date_str, '%Y-%m-%d').date()
-            dietician = Dietician.objects.get(pk=pk)
-        except Dietician.DoesNotExist:
-            return Response({'error': 'Diyetisyen seçilmeli'}, status=status.HTTP_400_BAD_REQUEST)
-        except ValueError:
-            return Response({'error:' 'Tarih formatı yanlış'})
+            dietician = Dietician.objects.get(pk=dietician)
+
+        except (ValueError, Dietician.DoesNotExist):
+            return Response({'error': 'Tarih formatı yanlış veya diyetisyen bulunamadı'},
+                            status=status.HTTP_400_BAD_REQUEST)
 
         start_time = dietician.work_time_start
         end_time = dietician.work_time_end
         duration = dietician.appointment_duration
 
         booked_slots = Appointment.objects.filter(
-            dietician_id=pk,
+            dietician=dietician,
             date=target_date,
             status__in=['SCHEDULED', 'COMPLETED'],
         )
-
+        if booked_slots.exists():
+            print("TYPE:", type(booked_slots.first().start_time))
+        else:
+            print("Hiç randevu yok")
+        booked_times = set(appt.start_time.strftime('%H:%M') for appt in booked_slots)
         slots=[]
 
         current_date_time = datetime.combine(target_date, start_time)
         end_date_time = datetime.combine(target_date, end_time)
 
         while current_date_time + timedelta(minutes=duration) <= end_date_time:
-            slot_time = current_date_time.time()
-
-            is_booked = slot_time in booked_slots
+            formatted_time = current_date_time.strftime('%H:%M')
+            is_booked = formatted_time in booked_times
 
             slots.append({
-                'time': slot_time.strftime('%H:%M'),
+                'time': formatted_time,
                 'is_available': not is_booked,
             })
             current_date_time += timedelta(minutes=duration)
