cb1f8c1d42623bbdafcee27c250885a2979ab54b
BerkeBoran <2022141046@cumhuriyet.edu.tr>
2026-04-16 15:06:21 +0300

    chore(admin): update appointments admin config

--- backend/apps/appointments/admin.py
+++ backend/apps/appointments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from apps.appointments.models.appointment import Appointment
+
+
 # Register your models here.
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('dietician', 'client', 'date', 'start_time')
