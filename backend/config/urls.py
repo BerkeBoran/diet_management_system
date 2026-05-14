48ec8de5a02f9bd6aaac23621f92a2cc569b57ab
BerkeBoran <2022141046@cumhuriyet.edu.tr>
2026-05-14 19:34:24 +0300

    fix(urls): add missing url patterns

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,6 +16,10 @@ urlpatterns = [
     path("api/auth/", include("dj_rest_auth.registration.urls")),
     path('api/subscription/', include('apps.subscription.urls')),
     path('api/payment/', include('apps.payment.urls')),
+    path('signup-dummy/', TemplateView.as_view(template_name="dummy.html"), name='account_signup'),
+    path('login-dummy/', TemplateView.as_view(template_name="dummy.html"), name='account_login'),
+    path('password-reset/', TemplateView.as_view(template_name="dummy.html"), name='account_reset_password'),
+
 
 ]
 if settings.DEBUG:
